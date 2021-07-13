c66f5c1107aa207114be619b5409b47d5fcdeb96
vvduniverse <86676363+vvduniverse@users.noreply.github.com>
2021-07-13 13:46:13 +0300

    Added some outlets and actions

--- userLoginApp/LoginViewController.swift
+++ userLoginApp/LoginViewController.swift
@@ -3,11 +3,20 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var userLoginTF: UITextField!
+    @IBOutlet weak var userPasswordTF: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
-
+    @IBAction func loginButton() {
+    }
+    
+    @IBAction func userLoginHelpButton() {
+    }
+    
+    @IBAction func userPasswordHelpButton() {
+    }
 }
 
