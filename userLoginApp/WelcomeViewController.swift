4451b745c16c275b6d3f049a59fcb69d36aee484
vvduniverse <86676363+vvduniverse@users.noreply.github.com>
2021-07-13 14:02:57 +0300

    Visual design plus actions and outlets.

--- userLoginApp/WelcomeViewController.swift
+++ userLoginApp/WelcomeViewController.swift
@@ -3,10 +3,13 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 
+    @IBOutlet weak var welcomeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
-
+    @IBAction func logoutTapped() {
+    }
+    
 }
