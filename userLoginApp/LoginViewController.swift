1276e9ade43a599df6fcd2bfe48a137fee28dd82
vvduniverse <86676363+vvduniverse@users.noreply.github.com>
2021-07-13 15:19:39 +0300

    Login segue is ready.

--- userLoginApp/LoginViewController.swift
+++ userLoginApp/LoginViewController.swift
@@ -18,5 +18,12 @@ class LoginViewController: UIViewController {
     
     @IBAction func userPasswordHelpButton() {
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        welcomeVC.username = userLoginTF.text
+    }
+    
+    
 }
 
--- userLoginApp/WelcomeViewController.swift
+++ userLoginApp/WelcomeViewController.swift
@@ -3,10 +3,14 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 
+    var username: String?
+    
     @IBOutlet weak var welcomeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        guard let username = self.username else { return }
+        welcomeLabel.text = "Welcome, \(username)!"
     }
     
     @IBAction func logoutTapped() {
